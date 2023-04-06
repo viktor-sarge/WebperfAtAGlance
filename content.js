1765d8f75d321c78804bb8aae1fcdf480fd8ae2e
Viktor Sarge <viktor.sarge@regionhalland.se>
2023-04-06 11:47:41 +0300

    feat(app): ✨ Add color coding of detail pages on webperf.se
    Lagt till färgkodade betyg på webperf.se:s detaljsidor

---
+++ content.js
@@ -0,0 +1,42 @@
+(function () {
+    // Get all the paragraphs within the entry-excerpt div
+    const paragraphs = document.querySelectorAll('.entry-excerpt p');
+
+    // Loop through each paragraph
+    paragraphs.forEach(function (paragraph) {
+        // Get the inner text of the paragraph and split it by line break
+        const lines = paragraph.innerText.split('\n');
+
+        // Create a new array to hold the new lines with span tags
+        const newLines = [];
+
+        // Loop through each line
+        lines.forEach(function (line) {
+        // If the line has content
+        if (line.trim().length > 0) {
+            // Create a new span element
+            const span = document.createElement('span');
+
+            // Set the text content of the span to the line
+            span.textContent = line;
+
+            // Check the line value and set the appropriate style
+            const value = parseFloat(line.match(/\d+\.\d+/));
+            if (value >= 1 && value <= 2.99) {
+            span.style.color = 'red';
+            } else if (value >= 3 && value <= 3.99) {
+            span.style.color = 'darkgoldenrod';
+            }
+
+            // Add the span element to the new lines array
+            newLines.push(span.outerHTML);
+        } else {
+            // If the line is empty, add it to the new lines array
+            newLines.push(line);
+        }
+        });
+
+        // Join the new lines with the br tag and set the paragraph innerHTML
+        paragraph.innerHTML = newLines.join('<br>');
+    });
+})();
