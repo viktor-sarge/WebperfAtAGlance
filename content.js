f23edc74955f03384b128b8d5e373f43f280db74
Viktor Sarge <viktor.sarge@regionhalland.se>
2023-04-17 11:03:14 +0300

    fix(app): 🐛 keep html
    Behåll html

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// Merges lines with only a grade as content with the previous line so the whole context gets colour coded
 function mergeNodesWithParenthesisContent(arr) {
     for (let i = 0, merged = false; i < arr.length; i++) {
         if (merged) {
@@ -17,15 +18,14 @@ function mergeNodesWithParenthesisContent(arr) {
     return arr;
 }
 
-
 (function () {
     // Get all the paragraphs within the entry-excerpt div
     const paragraphs = document.querySelectorAll('.entry-excerpt');
 
     // Loop through each paragraph
     paragraphs.forEach(function (paragraph) {
-        // Get the inner text of the paragraph and split it by line break
-        let lines = paragraph.innerText.split('\n');
+        // Get the innerHTML of the paragraph and split it by line break
+        let lines = paragraph.innerHTML.split('\n');
         lines = mergeNodesWithParenthesisContent(lines);
 
         // Create a new array to hold the new lines with span tags
@@ -33,32 +33,32 @@ function mergeNodesWithParenthesisContent(arr) {
 
         // Loop through each line
         lines.forEach(function (line) {
-        // If the line has content
-        if (line.trim().length > 0) {
-            // Create a new span element
-            const span = document.createElement('span');
+            // If the line has content
+            if (line.trim().length > 0) {
+                // Create a new span element
+                const span = document.createElement('span');
 
-            // Set the text content of the span to the line
-            span.textContent = line;
+                // Check the line value and set the appropriate style
+                const match = line.match(/\(\s*(\d+\.\d+)\s*betyg\s*\)/);
+                const value = match ? parseFloat(match[1]) : NaN;
+                if (value >= 1 && value <= 2.99) {
+                    span.style.color = 'red';
+                } else if (value >= 3 && value <= 3.99) {
+                    span.style.color = 'darkgoldenrod';
+                }
 
-            // Check the line value and set the appropriate style
-            const match = line.match(/\(\s*(\d+\.\d+)\s*betyg\s*\)/);
-            const value = match ? parseFloat(match[1]) : NaN;
-            if (value >= 1 && value <= 2.99) {
-                span.style.color = 'red';
-            } else if (value >= 3 && value <= 3.99) {
-                span.style.color = 'darkgoldenrod';
+                // Set the innerHTML of the span to the line
+                span.innerHTML = line;
+
+                // Replace the original line with the styled span in the new lines array
+                newLines.push(span.outerHTML);
+            } else {
+                // If the line is empty, add it to the new lines array
+                newLines.push(line);
             }
-
-            // Add the span element to the new lines array
-            newLines.push(span.outerHTML);
-        } else {
-            // If the line is empty, add it to the new lines array
-            newLines.push(line);
-        }
         });
 
-        // Join the new lines with the br tag and set the paragraph innerHTML
-        paragraph.innerHTML = newLines.join('<br>');
+        // Join the new lines and set the paragraph innerHTML
+        paragraph.innerHTML = newLines.join('');
     });
 })();
